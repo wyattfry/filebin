3a7a454379ec99b8a4f5fe848d9c0a544bedbf4e
Wyatt Fry <wyattfry@gmail.com>
2022-01-14 21:49:57 -0500

    make file dir

--- app.js
+++ app.js
@@ -17,26 +17,24 @@ const app = express();
 
 
 const apiLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 1, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 1, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 app.use('/upload', apiLimiter)
 
-
-
 // middleware
 app.use(morgan('dev'));
 app.use((req, res, next) => {
-  const length = parseInt(req.header('content-length'), 10);
-  if (length > MAX_FILE_SIZE) {
-    res.status(400);
-    res.statusText = `File size ${humanReadable.fromBytes(length)} exceeds max allowed ${humanReadable.fromBytes(MAX_FILE_SIZE)}`;
-  } else {
-    next();
-  }
+    const length = parseInt(req.header('content-length'), 10);
+    if (length > MAX_FILE_SIZE) {
+        res.status(400);
+        res.statusText = `File size ${humanReadable.fromBytes(length)} exceeds max allowed ${humanReadable.fromBytes(MAX_FILE_SIZE)}`;
+    } else {
+        next();
+    }
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // this is to handle URL encoded data
@@ -46,84 +44,90 @@ app.use(express.static(path.join(__dirname, 'public'))); // enable static files 
 
 // routes
 app.get('/download/:keywords', (request, response) => {
-  const keywords = `${request.params.keywords}.`
-    .trim()
-    .toLocaleLowerCase()
-    .substring(0, 50)
-    .split(' ')
-    .join('.');
+    const keywords = `${request.params.keywords}.`
+        .trim()
+        .toLocaleLowerCase()
+        .substring(0, 50)
+        .split(' ')
+        .join('.');
 
-  // TODO validate
-  const filedir = path.join(__dirname, 'files');
-  fs.readdir(filedir, (readdirerr, files) => {
-    if (readdirerr) {
-      console.error('failed to read directory', readdirerr);
-      response.statusText(JSON.stringify(readdirerr));
-      response.status(500);
-      response.end();
-      return;
-    }
-    const filename = files.filter((x) => x.startsWith(keywords))[0];
-    if (!filename) {
-      response.statusCode = 404;
-      response.end();
-      return;
-    }
-    const originalName = filename.split('.').slice(3).join('.'); // remove keywords prefix to get original filename
-    const filepath = path.join(__dirname, 'files', filename);
-    response.download(filepath, originalName, (downloaderr) => {
-      response.status(500, downloaderr);
-      response.end();
+    // TODO validate
+    const filedir = path.join(__dirname, 'files');
+    fs.readdir(filedir, (readdirerr, files) => {
+        if (readdirerr) {
+            console.error('failed to read directory', readdirerr);
+            response.statusText(JSON.stringify(readdirerr));
+            response.status(500);
+            response.end();
+            return;
+        }
+        const filename = files.filter((x) => x.startsWith(keywords))[0];
+        if (!filename) {
+            response.statusCode = 404;
+            response.end();
+            return;
+        }
+        const originalName = filename.split('.').slice(3).join('.'); // remove keywords prefix to get original filename
+        const filepath = path.join(__dirname, 'files', filename);
+        response.download(filepath, originalName, (downloaderr) => {
+            response.status(500, downloaderr);
+            response.end();
+        });
     });
-  });
 });
 
 // HTTP POST
 // upload files to server
 app.post('/upload', (request, response) => {
-  if (request.files) {
-    const { file } = request.files;
-    const rwords = randomWords(3).join('.');
-    const filename = `${rwords}.${file.name}`;
-    const targetpath = path.join(__dirname, 'files', filename);
-    file.mv(targetpath)
-      .then(() => response.json(rwords));
-  }
+    if (request.files) {
+        const { file } = request.files;
+        const rwords = randomWords(3).join('.');
+        const filename = `${rwords}.${file.name}`;
+        const targetpath = path.join(__dirname, 'files', filename);
+        file.mv(targetpath)
+            .then(() => response.json(rwords));
+    }
 });
 
-
+try {
+    fs.mkdirSync('files', {});
+} catch (error) {
+    if (error.code !== 'EEXIST') {
+        console.error(error);
+    }
+}
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 
 // delete old files
 function deleteOldFiles() {
-  const filedir = path.join(__dirname, 'files');
-  fs.readdir(filedir, (readdirerr, files) => {
-    if (readdirerr) {
-      console.error('failed to read directory', readdirerr);
-      return;
-    }
-    for (let i = 0; i < files.length; i += 1) {
-      const file = path.join(__dirname, 'files', files[i]);
-      fs.stat(file, (staterr, stats) => {
-        if (staterr) {
-          console.error('failed to get status of file', file, staterr);
-          return;
+    const filedir = path.join(__dirname, 'files');
+    fs.readdir(filedir, (readdirerr, files) => {
+        if (readdirerr) {
+            console.error('failed to read directory', readdirerr);
+            return;
         }
-        const { birthtime } = stats;
-        const age = Date.now() - birthtime;
-        if (age > MAX_FILE_AGE_MS) {
-          fs.unlink(file, (unlinkerr) => {
-            if (unlinkerr) {
-              console.error('failed to delete file', file, unlinkerr);
-              return;
-            }
-            console.log('Deleted', file);
-          });
+        for (let i = 0; i < files.length; i += 1) {
+            const file = path.join(__dirname, 'files', files[i]);
+            fs.stat(file, (staterr, stats) => {
+                if (staterr) {
+                    console.error('failed to get status of file', file, staterr);
+                    return;
+                }
+                const { birthtime } = stats;
+                const age = Date.now() - birthtime;
+                if (age > MAX_FILE_AGE_MS) {
+                    fs.unlink(file, (unlinkerr) => {
+                        if (unlinkerr) {
+                            console.error('failed to delete file', file, unlinkerr);
+                            return;
+                        }
+                        console.log('Deleted', file);
+                    });
+                }
+            });
         }
-      });
-    }
-  });
+    });
 }
 
 setInterval(deleteOldFiles, DELETE_JOB_INTERVAL_MS);
